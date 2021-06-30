df4624e2b8fa88e040b6fc30335fcadedd4722ae
Vitalii Kovalenko <vitaliikovalenko23@gmail.com>
2021-07-01 00:09:05 +0300

    added toggle

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
+import listOfDishes from './list-of-dishes.json';
+import menuTemplate from '../src/templates/menu-template.hbs';
 import './sass/main.scss';
+import { even } from 'prelude-ls';
+
+const jsMenu = document.querySelector('.js-menu');
+
+jsMenu.innerHTML = menuTemplate(listOfDishes);
+
+const toolBarInput = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+let isLightTheme = true;
+
+toolBarInput.addEventListener('change', (event) => {
+    event.preventDefault();
+    if (isLightTheme) {
+        localStorage.setItem('Theme', Theme.DARK);
+        setValidClass('dark-theme', 'light-theme');
+    } else {
+        localStorage.setItem('Theme', Theme.LIGHT);
+        setValidClass('light-theme', 'dark-theme');
+    }
+
+    isLightTheme = !isLightTheme
+});
+
+function changeBodyTheme() {
+    const currentTheme = localStorage.getItem('Theme');
+
+    if (currentTheme) {
+        body.classList.add(currentTheme);   
+    }
+
+    if (currentTheme === Theme.DARK) {
+        toolBarInput.checked = true;
+     }
+};
+
+// function setValidClass (classToAdd, classToRemove) {
+//     body.classList.add(classToAdd);
+//     body.classList.remove(classToRemove);
+// }
+changeBodyTheme();
